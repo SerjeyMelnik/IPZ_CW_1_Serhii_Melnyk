f145535550c7e17f06c4be30b7d7079f13c1683a
serjeymelnik <serjeymelnik@gmail.com>
2024-03-11 14:27:18 +0200

    Added image

--- app/src/main/java/com/example/ipz_cw_1/MainActivity.kt
+++ app/src/main/java/com/example/ipz_cw_1/MainActivity.kt
@@ -3,12 +3,15 @@ package com.example.ipz_cw_1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.ipz_cw_1.ui.theme.IPZ_CW_1_Melnik_SerhiiTheme
 
@@ -20,9 +23,14 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = MaterialTheme.colorScheme.background,
+
+
                 ) {
-                    Greeting("Android")
+                    Column {
+                     Image(painter = painterResource(id = R.drawable.android_logo), contentDescription = "")
+                    }
+
                 }
             }
         }
